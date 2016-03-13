040a0e35323eb4f3a5bb2891d62072f820998a6d
Ryan Osgood <ryanleeosgood@gmail.com>
2016-03-13 12:07:04 -0400

    Added Readme and comments

---
+++ landmarks/README.md
@@ -0,0 +1,11 @@
+# README 
+
+* I believe the entire work has been implemented correctly.
+* I have not worked on this with anyone.
+* I have spent 12 hours on this assignment. 
+
+* Note: I have used the Haversine Implementation from the StackOverflow post Ming posted on the assignment sheet, the link is here: 
+	http://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript
+
+
+
--- landmarks/landmarks.js
+++ landmarks/landmarks.js
@@ -4,17 +4,21 @@ var MyLongitude = 0;
 var map;
 var request = new XMLHttpRequest
 
+
+//This function gets my current location and stores in the two MyLatitude and MyLongitude Variables
 function GetMyLocation(){
-	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+	if (navigator.geolocation) { //this checks that geolocation is supported on the browser.
 		navigator.geolocation.getCurrentPosition(function(position) {
 			MyLatitude = position.coords.latitude;
 			MyLongitude = position.coords.longitude;
 			Setup();
 			});
 	}else{
-		//do someting
+		alert("Geolocation is not supported on this browser! Sorry!");
 	}
 }
+
+//This function sends my data to Ming's API and when the JSON is received back, it calls the StartMap function.
 function Setup(){
 	var my_info = "login=AMOS_HORN&lat="+MyLatitude+"&lng="+MyLongitude;
 	request.open("POST", "https://defense-in-derpth.herokuapp.com/sendLocation", true);
@@ -25,21 +29,24 @@ function Setup(){
 	request.onreadystatechange = StartMap;
 }
 
+
+//This function configures and create the map and displays it, it then calls the PopulateMap function to show markers
 function StartMap(){
 	var my_location = new google.maps.LatLng(MyLatitude, MyLongitude);
-	var marker;
 	
 	var map_options = {
-						zoom: 13, // The larger the zoom number, the bigger the zoom
+						zoom: 15,
 						center: my_location,
 						mapTypeId: google.maps.MapTypeId.ROADMAP
-
 						};
 	map = new google.maps.Map(document.getElementById("landmark_map"), map_options);
-	DisplayMap();
+	PopulateMap();
 }
-function DisplayMap(){
+
+//the following function puts all the necessary markers on the map at the correct locations and draws the polyline.
+function PopulateMap(){
 	
+	//the following code is used to create the icons for the 3 types of markers (mine, people, landmarks)
 
 	var my_icon = {
     url: "me.png", // url
@@ -61,35 +68,32 @@ function DisplayMap(){
     	anchor: new google.maps.Point(0, 0) // anchor
 	};
 
-
-	my_location = new google.maps.LatLng(MyLatitude,MyLongitude);
+	
+	//this creates an object that contains information on the closest landmark.
 	var ClosestLandmark = {
-			distance: 1,
+			distance: 1, //distance is set to 1 instead of 0 as the furthest distance is going to be max. 1
 			lat: 0,
 			lon: 0,
 			title: ""
 		}
 				
-				// Update map and go there..
-	map.panTo(my_location);
-
-
-	
-	
-
 	if (request.readyState == 4 && request.status == 200) {
 		marker_data = JSON.parse(request.responseText);
 		
 		var infowindow = new google.maps.InfoWindow();
+
+		//this for loop is used to position all the people markers on the map.
 		for (var i = 0; i < marker_data.people.length; i++) {
-	
+			//this if statement makes sure my location does not have two markers
 			if (marker_data.people[i].lat != MyLatitude && marker_data.people[i].lng != MyLongitude) {
 				locations = new google.maps.LatLng(marker_data.people[i].lat,marker_data.people[i].lng);
+				//create a marker
 				marker = new google.maps.Marker({
 							position: locations,
 							title: marker_data.people[i].login,
 							icon: student_icon,
 							map: map,
+							//I decided to round the distance to 2 decimal places. 
 							content:marker_data.people[i].login + " <br>Miles away: " + Math.round(Haversine(MyLatitude, MyLongitude, marker_data.people[i].lat,marker_data.people[i].lng) * 100) / 100
 							});
 				marker.addListener('click', function() {
@@ -99,10 +103,11 @@ function DisplayMap(){
 
 			}	
 		}
+		//this for loop displays all the landmarks in the area and keeps track of the closest one.
 		for (var i = 0; i < marker_data.landmarks.length; i++) {
 			locations = new google.maps.LatLng(marker_data.landmarks[i].geometry.coordinates[1],marker_data.landmarks[i].geometry.coordinates[0]);
 			
-			//the following code finds the closest landmark to my location
+			//the following code finds the closest landmark from my location and updates ClosestLandmark
 			var TempDistance;
 			TempDistance = Haversine(MyLatitude, MyLongitude, marker_data.landmarks[i].geometry.coordinates[1],marker_data.landmarks[i].geometry.coordinates[0]);
 			if (TempDistance < ClosestLandmark.distance) {
@@ -111,7 +116,6 @@ function DisplayMap(){
 				ClosestLandmark.lon = marker_data.landmarks[i].geometry.coordinates[0]
 				ClosestLandmark.title = marker_data.landmarks[i].properties.Location_Name
 			}
-			
 			marker = new google.maps.Marker({
 							position: locations,
 							title: marker_data.landmarks[i].properties.Location_Name,
@@ -125,13 +129,14 @@ function DisplayMap(){
   					});
 		}
 
-		// Create and place my marker
+		// This code creates my marker and displays the relevant information on the infowindow.
+		my_location = new google.maps.LatLng(MyLatitude,MyLongitude);
 		marker = new google.maps.Marker({
 							position: my_location,
 							title: "You Found me!",
 							icon: my_icon, 
 							map: map,
-							content: "That's Me!<br>" + "The closest landmark is <b> " + ClosestLandmark.title +"</b><br> and it's <b>"+ Math.round(ClosestLandmark.distance*100)/ 100 + "</b> miles away."
+							content: "That's Me!<br>" + "The closest landmark is <b> " + ClosestLandmark.title +"</b><br> and it's <b>"+ Math.round(ClosestLandmark.distance*100)/ 100 + " miles</b> away."
 							});
 			//create an infowindow
 		var infowindow = new google.maps.InfoWindow();
@@ -141,6 +146,7 @@ function DisplayMap(){
 			});
 
 
+		//this code is used to display the polyline between my location and the closest landmark.
 		var ClosestLandmarkPath = [
     		{lat: MyLatitude, lng: MyLongitude},
     		{lat: ClosestLandmark.lat, lng: ClosestLandmark.lon},
@@ -159,7 +165,7 @@ function DisplayMap(){
 	}
 }
 
-
+//the following Haversine function and helper are used to calculate distances using geolocation(lat,lon)
 //  the haversine function and helper function has been taking from stackoverflow at the following url:
 //  http://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript
 Number.prototype.toRad = function() {
