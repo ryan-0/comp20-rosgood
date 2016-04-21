c05e84f7104406933b6bacbe7b20a8423920d185
Ryan Osgood <ryanleeosgood@gmail.com>
2016-04-21 13:35:33 -0400

    adding assignment 4 folder and partner backend code

--- messages/lab.js
+++ messages/lab.js
@@ -1,9 +1,6 @@
-
 request = new XMLHttpRequest();
 request.open("GET", "data.json", true);
 request.onreadystatechange = parse;
-	
-
 function parse () {
 			elem = document.getElementById("messages");
 			if (request.readyState == 4 && request.status == 200) {
@@ -11,9 +8,7 @@ function parse () {
 				elem.innerHTML = "<h3>" + text_message[0]["content"]+"</h3><p> -"+ text_message[0]["username"]+"</p>"+ "<h3>" + text_message[1]["content"]+"</h3><p> -"+ text_message[1]["username"]+"</p>";
 			}
 			else if (request.readyState == 4 && request.status != 200) {
-				
 				elem.innerHTML = "<p>Whoops, this means I'm going to fail this lab!</p>";
 			}
 		};
-
 request.send(null); 
